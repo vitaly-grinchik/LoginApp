680846917f23d3b81ada2c1048bc46e9129d89d7
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2022-12-21 14:11:54 +0300

    Model creation

---
+++ LoginApp/Models/Account.swift
@@ -0,0 +1,50 @@
+//
+//  Account.swift
+//  LoginApp
+//
+//  Created by Виталий Гринчик on 21.12.22.
+//
+
+struct Account {
+    let userName: String
+    let userPassword: String
+    
+    let isActive: Bool
+    let recoveryQuestion: String
+    let recoveryAnswer: String
+    
+    let userInfo: Person
+}
+
+struct Person {
+    let name: String
+    let surname: String
+    let age: Int
+    let contact: Contact
+    var intro: String
+}
+
+struct Contact {
+    let email: String
+    let phone: [PhoneNumber]
+    let address: Address
+}
+
+struct PhoneNumber {
+    let number: String
+    let type: PhoneType
+}
+
+struct Address {
+    let state: String
+    let city: String
+    let street: String
+    let building: String
+    let appartment: String
+}
+
+enum PhoneType {
+    case cell
+    case home
+    case work
+}
